36daac1398c95243cfa438c8cd3a093e0aa4dca8
maggie-tutorials <maggie.dot.magpie@gmail.com>
2017-08-12 16:48:00 +0200

    Created user record on Google+ authentication

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose; // ES6 destructuring const Schema = mongoose.Schema;
+// mongoose wants to know all the different properties that our Collection might have
+
+const userSchema = new Schema({
+  googleId: String // tells our Schema that this value will always be a String
+});
+
+mongoose.model('users', userSchema);
+// mongoose will not override existing collections, it will only create it
+// if it doesn't exist
