860e08c34f4ab0d23889fac5a7e9dbdd0d000981
mserino <m.serino@hotmail.it>
2017-08-13 17:33:24 +0200

    Added basic routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
+// Provider tag is a react component that knows how to read changes from a redux store.
+// Every time the redux store gets any changes, the Provider informs all the components.
